81738da938f7239beda321b3ff35b386bce634bf
RishiKr3101 <rishikrofficial@gmail.com>
2021-04-13 20:26:27 +0530

    Done

--- static/js/app.js
+++ static/js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#000000"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#000000",
         "opacity": 0.4,
         "width": 1
       },
